a143f6fbdf65a6f741232fbd5a873b90c23004ba
Neemias Cormino Manso <130704790+n33miaz@users.noreply.github.com>
2025-11-07 12:19:26 -0300

    refactor(run_python)

--- config.py
+++ config.py
@@ -1,7 +1,5 @@
 SOURCE_DIR = r"E:\Intelbras\LocalRecording"
-
 BACKUP_DIR = r"D:\Intelbras\LocalRecording"
+LOG_DIR = r"D:\Logs"
 
-LOG_FILE = r"D:\Logs\backup_log.log"
-
-RETENTION_DAYS = 30
+RETENTION_DAYS = 31
